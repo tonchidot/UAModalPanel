fca48f18096c2ce14e0baf5b592e5c5d9a1ebe73
Makoto Hamanaka <m-hamanaka@tonchidot.com>
2012-05-14 21:37:22 +0900

    fixed pragma mark capitalization

--- UAModalPanel/Panel/Categories/UIView+JMNoise.m
+++ UAModalPanel/Panel/Categories/UIView+JMNoise.m
@@ -11,8 +11,8 @@
 
 #define JM_SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
 
-#pragma Mark -
-#pragma Mark - Noise Layer
+#pragma mark -
+#pragma mark - Noise Layer
 
 @interface NoiseLayer : CALayer
 + (UIImage *)noiseTileImage;
@@ -93,8 +93,8 @@ static UIImage * JMNoiseImage;
 
 @end
 
-#pragma Mark -
-#pragma Mark - UIView implementations
+#pragma mark -
+#pragma mark - UIView implementations
 
 @implementation UIView (JMNoise)
 
